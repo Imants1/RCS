be1f805e6414f9475de5ad19460d64633bf4aee9
Imants1 <36454544+Imants1@users.noreply.github.com>
2018-02-13 21:39:46 +0200

    kods

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //izsaucam sevis izveidoto funkciju
+            SayHello();
+            //izsaucam otru izveidoto funkciju
+            SayHelloToUser();
+            SayHelloAge();
+        }
+
+        static void SayHelloToUser()
+        {
+            //palūdzam lietotājam ierakstīt vārdu
+            Console.WriteLine("Please enter your name:");
+            //izveidojam mainīgo, kur glabāt lietotāja vērtību
+            string usersName;
+            //paprasam konsolei izvadīt
+            usersName = Console.ReadLine();
+            Console.WriteLine("Labdien," + usersName + "!");
+            
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+
+        static void SayHelloAge()
+        {
+            Console.WriteLine("Your age?");
+            string usersAge;
+            usersAge = Console.ReadLine();
+            Console.WriteLine(usersAge + ", that's good!");
+            Console.ReadLine();
+        }
+    }
+}
