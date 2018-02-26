a28b3a6cab3de3cb7a01c8cde732462711de3a25
Imants1 <36454544+Imants1@users.noreply.github.com>
2018-02-26 21:30:38 +0200

    weblapa

--- AdvertsWebApp/Controllers/HomeController.cs
+++ AdvertsWebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using AdvertsWebApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;//MVC=models, view, controller
@@ -15,7 +16,7 @@ namespace AdvertsWebApp.Controllers
             advertDb = new AdvertDb();    //izsauc konstruktoru, kas uztaisa datubāzi      
         }
 
-        private List<Advert> adverts;
+
         private AdvertDb advertDb;
         public ActionResult Index() // ja izsauc lapu bez īpašiem parametriem - izsauc Index lapu
         {
@@ -24,21 +25,33 @@ namespace AdvertsWebApp.Controllers
 
         public ActionResult Advert(int advertId)
         {
+
+            Advert ad = GetAdvertFromDb(advertId);
+            return View(ad);
+
+        }
+
+        private Advert GetAdvertFromDb(int advertId)
+        {
+
             foreach (var ad in advertDb.Adverts)
             {
                 if (ad.AdvertId == advertId)
                 {
-                    return View(ad);
+                    return ad;
                 }
-            }
-            return View();
 
+            }
+            return null;
         }
 
         public ActionResult CreateAdvert()
         {
-            return View();
+            return View();// ja šeit neko nenorāda, uzskata, ka jāatgriež skats ar nosaukumu, kā funkcijai
+            //ja grib var norādīt konkrētu view, ko izvadīt
         }
+
+        // http methods!!!
         // GET metode - iegūst datus no servera
         // POST metode - nosūta datus serverim. tā datus no formas var nosūtīt serverim
         [HttpPost] // automātiski uzskata GET, ja grib POST - jānorāda
@@ -49,5 +62,22 @@ namespace AdvertsWebApp.Controllers
             advertDb.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult EditAdvert(int advertId)
+        {
+            Advert editableAdvert = GetAdvertFromDb(advertId);
+            return View(editableAdvert);
+
+        }
+        [HttpPost]
+        public ActionResult EditAdvert(Advert advert)
+        {
+            if (!ModelState.IsValid)
+            {
+                View(advert);
+            }
+            advertDb.Entry(advert).State = EntityState.Modified;
+            advertDb.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-}
+}// izdarot izmaiņas aplikācijā - Package Manager Console jāievada add-migration "kautkāds nosaukums", pēc tam update-database
--- AdvertsWebApp/Models/Advert.cs
+++ AdvertsWebApp/Models/Advert.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Web;
 
 namespace AdvertsWebApp.Models
@@ -6,9 +8,19 @@ namespace AdvertsWebApp.Models
     public class Advert
     {
         public int AdvertId { get; set; }
+        //ar DisplayName nosakām kā lauks jānosauc, to izmantos arī labelfor un validationfor funkcijas no html
+        [DisplayName("Sludinājuma nosaukums")]// atribūts, kas raksturo lauku
+        [StringLength(100, MinimumLength = 3 )]
+        [Required(ErrorMessage = "Lauks {0} ir vajadzīgs")]
         public string Name { get; set; }
+
+        [DisplayName("Sludinājuma teksts")]
         public string Text { get; set; }
+
+        [DisplayName("Sludinājuma cena")]
+        [Required]// noteikts, ka laukam jābūt obligāti
         public double Price { get; set; }
+
         public DateTime CreationTime { get; set; }
 
     }
